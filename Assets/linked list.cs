879b987f5fd742ffaf62d0423116458b82afbd0e
Ripper4319 <filmguy4319@gmail.com>
2025-01-08 13:41:23 -0800

    linkeed list r hard

---
+++ Assets/linked list.cs
@@ -0,0 +1,17 @@
+public class node
+{
+    public char data;
+    public node nextnode;
+}
+
+public node(char d)
+{
+    data=di
+}
+
+public void insert(node head, node newnode)
+node search = head
+
+    for (;search.nextnode !=null)
+    searchnode = searchnode.nextnode;
+
