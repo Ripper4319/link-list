d66b365aebe54dcf37ebb72a0922b93ec15f2640
Ripper4319 <filmguy4319@gmail.com>
2025-01-26 13:19:38 -0800

    need to do stuff later

--- Assets/Scripts/Player/Player Movement.cs
+++ Assets/Scripts/Player/Player Movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-   
     [Header("Camera")]
     Vector2 camRotation;
     public float mouseSensitivity = 2.0f;
@@ -18,10 +17,14 @@ public class playerMovement : MonoBehaviour
     public int health = 20;
     public float speed = 5;
     public float jumpHeight = 6.5f;
+    public float groundDrag = 6f;
+    public float airMultiplier = 0.5f;
     public Transform cam1;
     private LayerMask mask;
     private Vector3 velocity;
     public bool isgrap = false;
+    private bool readyToJump = true;
+    public float jumpCooldown = 0.25f;
 
     [Header("Gun")]
     public LaserGun gun;
@@ -30,9 +33,12 @@ public class playerMovement : MonoBehaviour
     public GameObject Grapple;
     public GrapplingGun grap;
 
+    private bool grounded;
+
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        myRB.freezeRotation = true;
         respawnPos = new Vector3(0, -2, 0);
         zero = Quaternion.identity;
 
@@ -51,14 +57,106 @@ public class playerMovement : MonoBehaviour
             health = 20;
         }
 
+        HandleGrappling();
+        HandleShooting();
+        HandleCameraRotation();
+
+        grounded = IsGrounded();
+        HandleDrag();
+        ProcessMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void ProcessMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        Vector3 moveDirection = (right * horizontalInput + forward * verticalInput).normalized;
+
+        if (grounded)
+        {
+            velocity.x = moveDirection.x * speed;
+            velocity.z = moveDirection.z * speed;
+        }
+        else
+        {
+            velocity.x = moveDirection.x * speed * airMultiplier;
+            velocity.z = moveDirection.z * speed * airMultiplier;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump && grounded)
+        {
+            Jump();
+            readyToJump = false;
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        Vector3 flatVel = new Vector3(myRB.linearVelocity.x, 0f, myRB.linearVelocity.z);
+
+        if (flatVel.magnitude > speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * speed;
+            myRB.linearVelocity = new Vector3(limitedVel.x, myRB.linearVelocity.y, limitedVel.z);
+        }
+
+        myRB.linearVelocity = new Vector3(velocity.x, myRB.linearVelocity.y, velocity.z);
+    }
+
+    private void HandleDrag()
+    {
+        if (grounded)
+        {
+            myRB.linearDamping = groundDrag;
+        }
+        else
+        {
+            myRB.linearDamping = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        myRB.linearVelocity = new Vector3(myRB.linearVelocity.x, 0f, myRB.linearVelocity.z);
+        myRB.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void HandleCameraRotation()
+    {
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.timeScale;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.timeScale;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
+
+        transform.rotation = Quaternion.Euler(0, camRotation.x, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(-camRotation.y, 0, 0);
+    }
+
+    private void HandleGrappling()
+    {
         if (grap != null)
         {
             if (Input.GetMouseButtonDown(1))
             {
-                if (!isgrap && !grap.isin5flimit) 
+                if (!isgrap && !grap.isin5flimit)
                 {
-                   grap.StartGrapple();
-                   isgrap = true;
+                    grap.StartGrapple();
+                    isgrap = true;
                 }
                 else
                 {
@@ -68,15 +166,7 @@ public class playerMovement : MonoBehaviour
             }
         }
 
-        Stopgrap();
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (!isgrap)
-            {
-                gun.ShootLaser();
-            }
-        }
+        StopGrapple();
 
         if (Input.GetMouseButton(0))
         {
@@ -85,34 +175,14 @@ public class playerMovement : MonoBehaviour
                 grap.PullPlayer();
             }
         }
-        
+    }
 
-        velocity = myRB.linearVelocity;
-
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
-
-        Vector3 moveDirection = (right * horizontalInput + forward * verticalInput).normalized;
-
-        velocity.x = moveDirection.x * speed;
-        velocity.z = moveDirection.z * speed;
-
-        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.timeScale;
-        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.timeScale;
-
-        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
-
-        transform.rotation = Quaternion.Euler(-camRotation.y, camRotation.x, 0);
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0) && !isgrap)
         {
-            velocity.y = jumpHeight;
+            gun.ShootLaser();
         }
-
-        myRB.linearVelocity = velocity;
     }
 
     private bool IsGrounded()
@@ -128,8 +198,7 @@ public class playerMovement : MonoBehaviour
         }
     }
 
-
-    public void Stopgrap()
+    public void StopGrapple()
     {
         if (grap.isin5flimit)
         {
@@ -137,7 +206,5 @@ public class playerMovement : MonoBehaviour
             isgrap = false;
             grap.isin5flimit = false;
         }
-       
     }
 }
-
